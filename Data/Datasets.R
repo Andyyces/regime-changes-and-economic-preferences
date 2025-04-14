072cc592bb7d80009b0dab0a7dbc41a4bd8f1e55
Andyyces <andrea.ceskova@mailbox.tu-dresden.de>
2025-04-14 20:22:05 +0200

    🗃️ Add GPS dataset

--- Data/Datasets.R
+++ Data/Datasets.R
@@ -2,3 +2,11 @@
 #devtools::install_github("vdeminstitute/vdemdata")
 library(vdemdata)
 vdem <- vdemdata::vdem
+
+
+
+library(haven)
+library(here)
+unzip("Data/GPS_Dataset.zip")
+unzip("GPS_dataset_individual_level.zip")
+GPS_indiv <- read_dta("individual_new.dta")
