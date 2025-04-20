7726b259f58fa0777a7fee1830b0061877e0f293
memmedov200257 <elvin.mammadov@mailbox.tu-dresden.de>
2025-04-20 13:39:15 +0200

    :memo: adding Data wragling.Rmd

--- Data/Datasets.R
+++ Data/Datasets.R
@@ -1,5 +1,5 @@
-#install.packages("devtools")
-#devtools::install_github("vdeminstitute/vdemdata")
+install.packages("devtools")
+devtools::install_github("vdeminstitute/vdemdata")
 library(vdemdata)
 vdem <- vdemdata::vdem
 
