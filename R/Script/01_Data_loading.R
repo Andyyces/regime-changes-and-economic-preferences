877d29468947017cda3b2b944ec23c8fcfa9b228
Your <andrea.ceskova@mailbox.tu-dresden.de>
2025-06-16 09:47:51 +0200

    ♿️ downloading GDP data from dataverse

--- R/Script/01_Data_loading.R
+++ R/Script/01_Data_loading.R
@@ -12,16 +12,19 @@ p_load(haven,
        summarytools,
        devtools,
        zoo,
-       tidyverse)
+       tidyverse,
+       dataverse)
 
 #install.packages("summarytools")
 #install.packages("devtools")
 #devtools::install_github("vdeminstitute/vdemdata")
 
 
-#Vdem data 
+# Downloading Vdem data 
 vdem <- vdemdata::vdem
 
+
+# Downaloading GPS Data
 #opening datasets (individual survey)
 unzip(here("Input", "raw", "GPS_Dataset.zip"), 
       files = "GPS_dataset_individual_level.zip",
@@ -33,11 +36,20 @@ unzip(here("Input", "raw", "GPS_dataset_individual_level.zip"),
 
 GPS_indiv <- read_dta(here("Input", "raw", "individual_v11_new.dta"))
 
-#openning data polity
-p5 <- read_excel(here("Input", "p5v2018.xls"))
-gdp_data <- read_excel(here("Input", "raw", "mpd2023_web_2.xlsx"))
 #converting the column encodings to UTF-8
 names(GPS_indiv)
 GPS_indiv <- GPS_indiv %>%
   mutate(across(where(is.character), ~ iconv(., from = "", to = "UTF-8")))
+
+
+
+# Downloading GDP data from the Dataverse
+gdp_data <- get_dataframe_by_id(
+  fileid = 421302,
+  server = "dataverse.nl",
+  .f = function(x) read_excel(x, sheet = "Full data")
+)
+
+
+
 
